ea6269481e4d1488f6e05c9ae14c6b91597ac265
parabamoghv <parabamoghv@gmail.com>
2024-04-29 19:17:56 -0400

    Added a few more properties

--- DashedMonoids/ListProp.lean
+++ DashedMonoids/ListProp.lean
@@ -24,14 +24,14 @@ def up_List {X:Set S}(L:List X):List S:=by
   case nil=> exact []
   case cons=> exact (head.1)::tail_h
 
-example (a:S):[a]≠ []:=by
+theorem ListOne_neq_ListNil (a:S):[a]≠ []:=by
   simp only [ne_eq, not_false_eq_true]
 
-example (a:S)(L:List S):a::L≠ []:= by
+theorem ListOneOrMore_neq_ListNil (a:S)(L:List S):a::L≠ []:= by
   simp only [ne_eq, not_false_eq_true]
 
-example (a b:S)(L:List S):a::b::L≠ []:=by
+theorem ListTwoOrMore_neq_ListNil (a b:S)(L:List S):a::b::L≠ []:=by
   simp only [ne_eq, not_false_eq_true]
 
-example (a b:S)(L:List S):∀ x:S, a::b::L≠ [x]:=by
+theorem ListTwoOrMore_neq_ListOne (a b:S)(L:List S):∀ x:S, a::b::L≠ [x]:=by
   simp only [ne_eq, cons.injEq, and_false, not_false_eq_true, implies_true]
